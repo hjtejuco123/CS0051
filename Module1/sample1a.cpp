c98b3babeecfb22a9fc0a209fdf0565ffe837692
hjtejuco123 <hadji.tejuco@gmail.com>
2025-04-25 08:24:01 +0800

    Create sample1a.cpp

---
+++ Module1/sample1a.cpp
@@ -0,0 +1,18 @@
+//sequential computing
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "sequential computing " << endl;
+    
+    int task1 = 5;
+    int task2 = 10;
+    int task3 = 15;
+    
+    cout << "Executing Task 1: Add 1 " << task1 + 1 << endl;
+    cout << "Executing Task 2: Multiply by 2 " << task2 * 2 << endl;
+    cout << "Executing Task 3: Subtract by 5 " << task3 - 5 << endl;
+    
+    return 0;
+}
