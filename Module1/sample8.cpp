61aec0ac5b300fd91bc8fe576aa9b3f960cd897b
hjtejuco123 <hadji.tejuco@gmail.com>
2025-05-02 12:18:41 +0800

    Create sample8.cpp

---
+++ Module1/sample8.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void simulateOperation(string opName, int a, int b);
+
+int main()
+{
+    cout << "Main thread " << this_thread::get_id() << "\n"<<endl;
+    int a = 20, b = 4;
+    
+    thread add(simulateOperation,"Addition",a,b);
+    thread sub(simulateOperation,"Subtraction",a,b);
+    thread mul(simulateOperation,"Multiplication",a,b);
+    thread divi(simulateOperation,"Division",a,b);
+    
+    add.join();
+    sub.join();
+    mul.join();
+    divi.join();
+    
+    return 0;
+}
+
+
+void simulateOperation(string opName, int a, int b)
+{
+    int result;
+    if (opName == "Addition") result = a+b;
+    else if (opName == "Subtraction") result = a-b;
+    else if (opName == "Multiplication") result = a*b;
+    else if (opName == "Division") result= a/b;
+    
+    cout << "Result: " << result 
+        << " (Thread ID : " << this_thread::get_id() << ") \n" << endl;
+    
+}
