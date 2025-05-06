2bfeb8e91e92881191e6c6c6b9e79a09b89f25c2
hjtejuco123 <hadji.tejuco@gmail.com>
2025-05-06 11:30:36 +0800

    Create sample1.cpp

---
+++ Module2/sample1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void task (string taskName);
+
+int main()
+{
+    cout << "Main thread id " << this_thread::get_id() <<endl;    
+    task("Task 1");
+    task("Task 2");
+    task("Task 3");
+    return 0;
+}
+
+void task (string taskName)
+{
+    //start the task -> get id -> sleep(2) -> done 
+    cout << " Starting Task " << taskName 
+        << " on thread ID " << this_thread::get_id() << " ... " << endl;
+    this_thread::sleep_for (chrono::second(2));
+    cout << taskName << "done ! " << endl; 
+    
+}
