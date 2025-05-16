924477cc7b53ad3aa392448a408cfa7cdef1b04b
hjtejuco123 <hadji.tejuco@gmail.com>
2025-05-16 12:34:16 +0800

    Update sample8.cpp

--- Module3/sample8.cpp
+++ Module3/sample8.cpp
@@ -22,11 +22,34 @@ void writer(int id) {
 }
 
 int main() {
-    thread readers[] = {thread(reader, 1), thread(reader, 2)};
-    thread writers[] = {thread(writer, 1), thread(writer, 2)};
+    //version 1
+    // thread readers[] = {thread(reader, 1), thread(reader, 2)};
+    // thread writers[] = {thread(writer, 1), thread(writer, 2)};
 
-    for (auto& t : readers) t.join();
-    for (auto& t : writers) t.join();
+    // for (auto& t : readers) t.join();
+    // for (auto& t : writers) t.join();
+    
+
+    //version 2
+     // Create reader threads
+    thread readers[2];
+    readers[0] = thread(reader, 1);
+    readers[1] = thread(reader, 2);
+
+    // Create writer threads
+    thread writers[2];
+    writers[0] = thread(writer, 1);
+    writers[1] = thread(writer, 2);
+
+    // Join reader threads
+    for (int i = 0; i < 2; i++) {
+        readers[i].join();
+    }
+
+    // Join writer threads
+    for (int i = 0; i < 2; i++) {
+        writers[i].join();
+    }
 
     return 0;
 }
